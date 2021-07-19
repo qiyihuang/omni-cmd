aaaef55d41eb88772461118967813dad189f2f2b
qiyi huang <46252024+qiyihuang@users.noreply.github.com>
2021-07-19 11:09:53 +1000

    chore/bump yaml from v2.4 to v3 (#12)
    * chore/bump yaml from v2.4 to v3
    
    * 0.2.1

--- cmd/omnicmd/main.go
+++ cmd/omnicmd/main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/qiyihuang/omni-cmd/server"
 )
 
-const version = "0.2.0"
+const version = "0.2.1"
 
 func main() {
 	if os.Getenv("ENV") != "production" && os.Getenv("ENV") != "test" {
